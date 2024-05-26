3abd4ed01ac18454496537fa1b52626182275ccc
wxxd-fxrest <fxrest704@gmail.com>
2024-05-26 17:11:50 +0900

    add SearchViewController

---
+++ TodaysWeather/Controllers/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  TodaysWeather
+//
+//  Created by 밀가루 on 5/26/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
